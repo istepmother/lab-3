337d3a39a2c0b7067c27bb1fdf40a92cc6669171
istepmother <imachukha@kse.org.ua>
2023-03-19 07:32:59 +0100

    added code examples

---
+++ lab3/LinkedList.cs
@@ -0,0 +1,21 @@
+﻿namespace lab3;
+
+public class LinkedList
+{
+    private LinkedListNode _first;
+
+    public void Add(KeyValuePair pair)
+    {
+        // add provided pair to the end of the linked list
+    }
+
+    public void RemoveByKey(string key)
+    {
+        // remove pair with provided key
+    }
+
+    public KeyValuePair GetItemWithKey(string key)
+    {
+        // get pair with provided key, return null if not found
+    }
+}
