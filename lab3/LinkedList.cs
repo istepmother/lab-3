0ae735e8054aa26ad0e03d67e040e8f9ba75aab0
istepmother <imachukha@kse.org.ua>
2023-03-31 10:00:16 +0200

    implemented linkedlist methods

--- lab3/LinkedList.cs
+++ lab3/LinkedList.cs
@@ -2,21 +2,114 @@
 
 public class LinkedList
 {
-    private LinkedListNode _first;
+    private LinkedListNode? _head;
 
-    public void Add(KeyValuePair pair)
+    private LinkedListNode? _tail;
+    private int _size;
+    
+    public LinkedList()
+    {
+        _tail = null;
+        _head = null;
+        _size = 0;
+    }
+    
+    public void AddBack(KeyValuePair pair)
     {
         // add provided pair to the end of the linked list
-		// just smth to check if it works properly
+        if (this._size == 0)
+        {
+            LinkedListNode cur = new LinkedListNode(pair);
+            _tail = cur;
+            _head = cur;
+            _size++;
+            return;
+        }
+        LinkedListNode notLastAnymore = _tail;
+        LinkedListNode last = new LinkedListNode(pair);
+        _tail = last;
+        notLastAnymore.Next = last;
+        _size++;
     }
-
+    
+    public void PrintLinkedList()
+    {
+        LinkedListNode? cur = _head;
+        for (int i = 0; i < _size; i++)
+        {
+            Console.WriteLine($"{cur.Pair.Key} and {cur.Pair.Value}");
+            cur = cur.Next;
+        }
+    }
     public void RemoveByKey(string key)
     {
         // remove pair with provided key
+        if (this._size == 0)
+        {
+            throw new Exception("cannot remove something from an empty list");
+        }
+
+        LinkedListNode? cur = this._head;
+        LinkedListNode? prev = null;
+        for (int i = 0; i < this._size; i++)
+        {
+            // check if the element is what we are looking for
+            var curPairKey = cur.Pair.Key;
+            if (key == curPairKey)
+            {
+                // remove this element and finish void
+                // if element is the first one
+                if (cur == _head)
+                {
+                    _head = _head.Next;
+                }
+                else
+                {
+                    prev.Next = cur.Next;
+                }
+
+                _size--;
+                // if we want to delete last element, we should change tail
+                if (cur.Next == null)
+                {
+                    _tail = prev;
+                }
+
+                return;
+            }
+
+            // move cur to the next, and change prev to the next
+            cur = cur.Next;
+            if (prev == null)
+            {
+                prev = _head;
+            }
+            else
+            {
+                prev = prev.Next;
+            }
+
+
+        }
+
+        throw new Exception("there is no such element in the list, so I cannot delete it");
     }
 
     public KeyValuePair GetItemWithKey(string key)
     {
         // get pair with provided key, return null if not found
+        var cur = _head;
+        for (int i = 0; i < _size; i++)
+        {
+            var curKey = cur.Pair.Key;
+            if (curKey == key)
+            {
+                return cur.Pair;
+            }
+
+            cur = cur.Next;
+        }
+
+        return null;
     }
 }
--- lab3/Program.cs
+++ lab3/Program.cs
@@ -1,29 +1,2 @@
 ﻿namespace lab3;
 
-public class StringsDictionary
-{
-    private const int InitialSize = 10;
-
-    private LinkedList[] _buckets = new LinkedList[InitialSize];
-        
-    public void Add(string key, string value)
-    {
-            
-    }
-
-    public void Remove(string key)
-    {
-            
-    }
-
-    public string Get(string key)
-    {
-        
-    }
-
-
-    private int CalculateHash(string key)
-    {
-        // function to convert string value to number 
-    }
-}
