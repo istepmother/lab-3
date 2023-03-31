c0c19fb52ad80ec05217ae01bcf86a14b85910d3
istepmother <imachukha@kse.org.ua>
2023-03-31 10:31:05 +0200

    demonstration of working dictionary

--- lab3/Program.cs
+++ lab3/Program.cs
@@ -1,4 +1,28 @@
-﻿namespace lab3;
+﻿using lab3;
 
-
-
+var myDictionary = new StringsDictionary();
+int counter = 0;
+string[] lines = File.ReadAllLines("C:/Users/Elisa/RiderProjects/lab3/lab3/dictionary.txt");
+foreach (var line in lines)
+{
+    counter++;
+    var wordDefArray = line.Split(";");
+    string word = wordDefArray[0];
+    string definition = string.Join("", wordDefArray, 1, wordDefArray.Length - 1);
+    myDictionary.Add(word, definition);
+}
+Console.WriteLine("hello! this is dictionary! amazing, isn't it? you can type in some word, and I will give you a definition");
+while (true)
+{
+    string input = Console.ReadLine();
+    string requestedWord = input.ToUpper();
+    try
+    {
+        string defenition = myDictionary.Get(requestedWord);
+        Console.WriteLine($"{requestedWord}   {defenition}");
+    }
+    catch
+    {
+        Console.WriteLine("there's no such word in our dictionary");
+    }
+}
